df542b6a60235c1e29a9f70cf289084f5d4a60a5
dgomarasad <dgomarasad@educacion.navarra.es>
2024-11-22 09:10:54 +0100

    Create Ejercicio03.py

---
+++ Ejercicio03.py
@@ -0,0 +1,7 @@
+#Escribir un programa que guarde en un diccionario los precios de los
+#artículos de la tabla, pregunte al usuario por un artículo, un número de 
+#unidades y muestre por pantalla el precio de esa cantidad de producto. 
+#Si el producto no está en el diccionario debe mostrar un mensaje informando
+# de ello.
+precios = {'Pan':'1,4','Huevos':'2,3','Cebolla':'0,85','Aceite':'4,35'}
+input('Que producto quieres y cuantas unidades:')
