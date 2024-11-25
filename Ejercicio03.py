8e973cf1b8af9285709fe864ff76b762f384172f
dgomarasad <dgomarasad@educacion.navarra.es>
2024-11-25 12:03:20 +0100

    Update Ejercicio03.py

--- Ejercicio03.py
+++ Ejercicio03.py
@@ -3,5 +3,11 @@
 #unidades y muestre por pantalla el precio de esa cantidad de producto. 
 #Si el producto no está en el diccionario debe mostrar un mensaje informando
 # de ello.
-precios = {'Pan':'1,4','Huevos':'2,3','Cebolla':'0,85','Aceite':'4,35'}
-input('Que producto quieres y cuantas unidades:')
+prdcts = {'Pan':1.4,'Huevos':2.3,'Cebolla':0.85,'Aceite':4.35}
+productos = input('Que producto quieres de la lista: ').capitalize()
+unidades = float(input("Dime cuantas unidades quieres del producto: "))
+if productos in prdcts:
+    precio = int(prdcts[productos] * unidades)
+    print(f"El precio de {productos} es de {precio :.2f}€ ")
+else:
+    print("El producto no está disponible")
